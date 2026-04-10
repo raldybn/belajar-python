b3452858fc7db636c143a6eee9869ce03ab43787
raldybn <raldybelo@gmail.com>
2026-04-11 01:04:35 +0700

    tambahan

--- latihan.py
+++ latihan.py
@@ -1,49 +1,27 @@
-# SOAL PERTAMA
-# #cek bilangan positif dan genap
-# Buat program yang meminta input bilangan bulat, lalu:
+print("#### INPUT NAMA MURID ####")
+class Murid:
+    def __init__(self):
+        self.namaMurid = input("NAMA MURID : \n").upper()
+        self.kelas = input("KELAS : \n").upper()
+        self.namaPemeriksa = input("NAMA PEMERIKSA : \n")
+        self.mataPelajaran = input("MATA PELAJARAN : \n")
+        self.jawabanBenar = True
+        self.jawabanSalah = False
+    
+    def info_murid(self):
+        print(f"\nNAMA MURID : {self.namaMurid}\nKELAS : {self.kelas}\n")
+        
+    def jawaban_murid(self):
+        self.soal_pertama = input("MASUKAN SOAL : \n")
+        self.jawaban = bool(input("JAWABAN : \n"))
+        if self.soal_pertama == self.jawaban:
+            print('JAWABAN ANDA BENAR')
+        else:
+            print("JAWABAN ANDA SALAH")
+      
+murid = Murid()
 
-# Tampilkan "Bilangan positif dan genap" jika bilangan lebih dari 0 dan habis dibagi 2.
-# Tampilkan "Tidak memenuhi syarat" jika tidak
-
-print("\nSOAL PERTAMA\n")
-userInput = int(input("masukan angka : "))
-
-angka = userInput
-#jika angka lebih dari 0 dan habis dibagi 2
-if angka > 0 and angka % 2 == 0:
-    print("Bilangan positif dan genap")
-else:
-    print("angka tidak memenuhi syarat")
-
-# SOAL 2 – Cek Usia untuk SIM
-# Buat program yang meminta input usia, lalu:
-
-# Jika usia ≥ 17 dan ≤ 65, tampilkan "Boleh membuat SIM".
-# Jika tidak, tampilkan "Tidak boleh membuat SIM".
-
-print("\nSOAL KEDUA\n")
-usia = int(input("berapa usia anda : "))
-
-if usia >= 17 and usia <= 62:
-    print("ANDA BISA MENGURUS SIM")
-else:
-    print("MAAF USIA ANDA TIDAK MASUK KRITERIA")
-
-# SOAL 3 – Login Sederhana
-# Buat program yang meminta username dan password:
-
-# Username benar jika "admin", password benar jika 1234.
-# Jika keduanya benar, tampilkan "Login berhasil".
-# Jika salah satu salah, tampilkan "Login gagal".
-
-print("\nSOAL KETIGA\n")
-try : 
-    userName = input("masukan username anda : ".lower())
-    password = int(input("masukan password anda : "))
-
-    if userName == "admin" and password == 1234:
-        print("anda berhasil login")
-    else :
-        print("login gagal")
-except :
-    print("password harus berupa angka")
+print("\n#### DATA MURID ####\n")
+murid.info_murid()
+print("\n#### KOREKSI ####\n")
+murid.jawaban_murid()
---
+++ operatorbitwise.py
@@ -0,0 +1,8 @@
+#operator bitwise
+
+a = 9
+b = 5
+
+#BITWISE OR (|)
+C = a | b
+print(C)
