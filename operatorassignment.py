753084d277849beecbb92ce00fcbd2a8fb8a8385
raldybn <raldybelo@gmail.com>
2026-04-11 02:13:15 +0700

    tambahan

---
+++ operatorassignment.py
@@ -0,0 +1,4 @@
+#operasi yang dapat dilakukan dengan penyingkatan
+#operasi ditambah dengan assignment
+
+a = 5 #ini adalah assignment
--- operatorbitwise.py
+++ operatorbitwise.py
@@ -4,5 +4,64 @@ a = 9
 b = 5
 
 #BITWISE OR (|)
-C = a | b
-print(C)
+c = a | b
+print(c)
+print("\n ===== OR ===== \n")
+print('nilai : ', a,', binary : ', format(a,"08b"))
+print('nilai : ', b,', binary : ', format(b,"08b"))
+print('------------------------------ (|)')
+print('nilai : ', c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+
+#BITWISE AND (&)
+c = a & b
+print(c)
+print("\n ===== AND ===== \n")
+print('nilai : ', a,', binary : ', format(a,"08b"))
+print('nilai : ', b,', binary : ', format(b,"08b"))
+print('------------------------------ (&)')
+print('nilai : ', c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+
+
+#BITWISE XOR (^)
+c = a ^ b
+print(c)
+print("\n ===== XOR ===== \n")
+print('nilai : ', a,', binary : ', format(a,"08b"))
+print('nilai : ', b,', binary : ', format(b,"08b"))
+print('------------------------------ (^)')
+print('nilai : ', c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+
+
+#BITWISE NOT(~)
+c = ~a
+print("\n ===== NOT ===== \n")
+print('nilai : ',a,', binary : ', format(a,"08b"))
+print('------------------------------ (~)')
+print('nilai : ',c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+print('------------------------------ (^)')
+d = 0b0000001001
+e = 0b11111111111
+print('nilai : ',e^d,', binary : ',format(e^d, '08b'))
+
+#SHIFTING
+
+#SHIFT RIGHT (>>)
+c = a >> 1
+print("\n ===== SHIFT RIGHT (>>) ===== \n")
+print('nilai : ',a,', binary : ', format(a,"08b"))
+print('------------------------------ (>>)')
+print('nilai : ',c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+
+#SHIFT LEFT (<<)
+c = a << 1
+print("\n ===== SHIFT LEFT (<<) ===== \n")
+print('nilai : ',a,', binary : ', format(a,"08b"))
+print('------------------------------ (<<)')
+print('nilai : ',c,', binary : ', format(c,"08b"))
+print(sep=("-"))
+
